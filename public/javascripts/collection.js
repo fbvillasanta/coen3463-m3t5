fa10de8fb3d5e5742e04ebeb3d9ace6b408d1752
fbvillasanta <fbvillasanta@yahoo.com>
2017-02-06 19:21:09 +0800

    change styles. allow username case insensitivity at login

--- public/javascripts/collection.js
+++ public/javascripts/collection.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
                 console.log('entries', entries);
                 var tbody = document.getElementById('thesis-index');
                 entries.forEach(function (entries) {
-                    tbody.insertAdjacentHTML('beforeend', '<div class="col-md-4 col-sm-4 animate-box fadeInUp animated-fast" data-animate-effect="fadeInUp"><div class="fh5co-post"><span class="fh5co-date">' + entries.added + '</span><h3><a href="/collection/' + entries._id + '">' + entries.thesis + '</h3><p>' + entries.sentence + '</p></div></div>');
+                    tbody.insertAdjacentHTML('beforeend', '<div class="col-md-4 col-sm-4 animate-box fadeInUp animated-fast" data-animate-effect="fadeInUp"><div class="fh5co-post"><span class="fh5co-date">' + entries.added + '</span><h3><a href="/collection/' + entries._id + '">' + entries.thesis + '</a></h3><p>' + entries.sentence + '</p></div></div>');
 
                 });
             })
@@ -18,12 +18,12 @@ $(document).ready(function() {
     }
 
     if (window.location.pathname === '/collection') {
-        fetch('api/v1/Thesis?sort=added').then(function(res) {
+        fetch('api/v1/Thesis?sort=_id').then(function(res) {
             res.json().then(function(entries) {
                 console.log('entries', entries);
                 var tbody = document.getElementById('thesis-body');
                 entries.forEach(function(entries) {
-                    tbody.insertAdjacentHTML('beforeend', '<div class="col-md-3 col-sm-4 animate-box fadeInUp animated-fast" data-animate-effect="fadeInUp"><div class="fh5co-post"><span class="fh5co-date">'+entries.added+'</span><h3><a href="/collection/' + entries._id + '">'+entries.thesis+'</h3><p>'+entries.sentence+'</p></div></div>' );
+                    tbody.insertAdjacentHTML('beforeend', '<div class="col-md-3 col-sm-4 animate-box fadeInUp animated-fast" data-animate-effect="fadeInUp"><div class="fh5co-post"><span class="fh5co-date">'+entries.added+'</span><h3><a href="/collection/' + entries._id + '">'+entries.thesis+'</a></h3><p>'+entries.sentence+'</p></div></div>' );
 
                 });
             })
@@ -32,7 +32,7 @@ $(document).ready(function() {
 
         fetch('api/v1/Thesis/count').then(function(res) {
             res.json().then(function(count) {
-                $('.countThesis #countTotal').html("A total of " + count.count+ " thesis can be viewed in our collection!");
+                $('#collectiontitle').html("<span><u>" + count.count+ "</u></span>  entries");
 
 
                 });
